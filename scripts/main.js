46156320a08c67176614f99ebb190e49298281f0
Dmitry <70842135+Ickem@users.noreply.github.com>
2020-09-22 14:27:12 +0300

    Added main menu and result menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
+            <!-- Game Menu -->
+const play = document.getElementById('gameMenuPlay');
+let menu = document.querySelector('.game-menu');
+let playMode = document.querySelector('.game-mode');
+play.addEventListener('click',()=>{
+    playMode.style.display='block';
+    menu.style.display='none';
+    createTask(arrOfTask);
+    createGameCube(countCubes, arrOfTask);
+});
 
+            <!-- Game Logic -->
 let arrOfTask = ['q','w','e','r','t','y'];
-const countElementsOfTask = arrOfTask.length;
-let taskMenu = document.getElementById('taskMenu'),
+    taskMenu = document.getElementById('taskMenu'),
     playMenu = document.getElementById('playMenu'),
     countCubes = 6;
+const countElementsOfTask = arrOfTask.length,
+      win = document.getElementById('win');
 
 function createGameCube(numberOfCubes, arr){
 
@@ -13,11 +25,16 @@ function createGameCube(numberOfCubes, arr){
 
         gameCube.addEventListener('click', ()=>{
             const taskList = taskMenu.querySelectorAll('.cube');
-            for(let task of taskList){
-                if(task.innerText === gameCube.innerText &&
-                    task.style.backgroundColor !='gray'){
+            for (let task of taskList) {
+                if (task.innerText === gameCube.innerText &&
+                    task.style.backgroundColor != 'gray'){
+
+                    if(playMenu.children.length == 1){
+                       return result('Win');
+                    }
+
                     return [gameCube.remove(),
-                            task.style.backgroundColor ='gray'];
+                            task.style.backgroundColor = 'gray'];
                 }
             }
         });
@@ -41,17 +58,33 @@ function createCube(itemofTask){
 }
 
 let countClick = 0;
+const lose = document.querySelector('.lose');
 document.getElementById('reroll').addEventListener('click',()=>{
     countClick++;
-    if(countClick > 0 && countClick <3){
+    if(countClick > 0 && countClick <5){
         let remainingCubes = playMenu.querySelectorAll('.cube');
         createGameCube(remainingCubes.length, arrOfTask);
 
         for(let cube of remainingCubes){
             cube.remove();
         }
-    } else console.log('You lose');
+    } else {
+        const exit = document.getElementById('exit');
+        exit.addEventListener('click',()=>{
+            lose.style.display='none';
+            menu.style.display='block';
+            countClick = 0;
+            taskMenu.innerHTML='';
+            playMenu.innerHTML='';
+        });
+        result('Lose');
+    }
 });
 
-createTask(arrOfTask);
-createGameCube(countCubes, arrOfTask);
+function result(result){
+    win.innerHTML = result;
+    playMode.style.display='none';
+    lose.style.display='block';
+}
+
+
